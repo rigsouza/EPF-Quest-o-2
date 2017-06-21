215022817f0de51caa21c2eee2ef11ea6535bf9f
Ricardo <r.souzagoncalves@gmail.com>
2017-06-20 22:27:33 -0300

    Questão 2 EPF

---
+++ scripts/processElementData.js
@@ -0,0 +1,91 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQ,_r_lRYVYaEee_gcc8A_7avQActivityDetail"]="./../../Questao2Plugin/deliveryprocesses/resources/SP 1.3 Estipular Táticas e Critérios de Checagem_B455DD85_fb5c923d_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQ,_psPVwVYaEee_gcc8A_7avQActivityDetail"]="./../../Questao2Plugin/deliveryprocesses/resources/SP 1.2 Estipular o Ambiente de Checagem_8D4C2A9D_d452df55_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQ,_vxzl0FYaEee_gcc8A_7avQActivityDetail"]="./../../Questao2Plugin/deliveryprocesses/resources/SP 2.2 Efetuar Inspeções por Pares_1189515F_58900617_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQ,_mtIgUVYaEee_gcc8A_7avQActivityDetail"]="./../../Questao2Plugin/deliveryprocesses/resources/SP 1.1 Eleger Produtos de Trabalho para Checagem_3DD7895B_84de3e13_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQ,_t4bCUVYaEee_gcc8A_7avQActivityDetail"]="./../../Questao2Plugin/deliveryprocesses/resources/SP 2.1 Organizar as Inspeções por Pares_781A7397_bf21284f_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQ,_zMs_YVYaEee_gcc8A_7avQActivityDetail"]="./../../Questao2Plugin/deliveryprocesses/resources/SP 3.1 Efetuar a Checagem_CCD642D1_13dcf789_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQActivity"]="./../../Questao2Plugin/deliveryprocesses/resources/ProcessoTrabalho2_3C7A1264_3c7a1264_Activity.jpeg"
+contentPage.processPage.imageFiles["_fDzXcVYaEee_gcc8A_7avQ,_xjtoUVYaEee_gcc8A_7avQActivityDetail"]="./../../Questao2Plugin/deliveryprocesses/resources/SP 2.3 Analisar Dados da Vistoria por Pares_DD1F0723_2425bbdb_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_Ff9CYFYdEee_gcc8A_7avQ"] = ["Apontar produtos de trabalho para checagem","Questao2Plugin/tasks/apontar_produtos_de_trabalho_para_checagem_DB7BAB38.html"];
+contentPage.processPage.elementUrls["_LMwNIFYdEee_gcc8A_7avQ"] = ["Definir os métodos de checagem a serem utilizados para cada produto de trabalho escolhido","Questao2Plugin/tasks/definir_os_mtodos_de_checagem_a_serem_utilizados_para_cada_produ_62ADCF6D.html"];
+contentPage.processPage.elementUrls["_lFMx0FYdEee_gcc8A_7avQ"] = ["Efetuar os papéis designados na vistoria por pares","Questao2Plugin/tasks/efetuar_os_papis_designados_na_vistoria_por_pares_7BD8B67D.html"];
+contentPage.processPage.elementUrls["_JpOk4VYdEee_gcc8A_7avQ"] = ["Analista de Qualidade","Questao2Plugin/roles/analista_de_qualidade_8BAFE9CD.html"];
+contentPage.processPage.elementUrls["_r03HwFYdEee_gcc8A_7avQ"] = ["Efetuar a checagem de produtos de trabalho contra seus critérios","Questao2Plugin/tasks/efetuar_a_checagem_de_produtos_de_trabalho_contra_seus_critrios_C5CFA7B0.html"];
+contentPage.processPage.elementUrls["_G6vlYVYdEee_gcc8A_7avQ"] = ["Analista de Sistemas","Questao2Plugin/roles/analista_de_sistemas_7DF399CF.html"];
+contentPage.processPage.elementUrls["_X9GTIFYdEee_gcc8A_7avQ"] = ["Criar um grupo de táticas de checagem amplo e integrado para os produtos de trabalho e produtos comerciais prontos, sobre análise de necessidade","Questao2Plugin/tasks/criar_um_grupo_de_tticas_de_checagem_amplo_e_integrado_para_os_p_CA6B92D1.html"];
+contentPage.processPage.elementUrls["_t4bCUVYaEee_gcc8A_7avQ"] = ["SP 2.1 Organizar as Inspeções por Pares","Questao2Plugin/deliveryprocesses/SP 2.1 Organizar as Inspeções por Pares_781A7397.html"];
+contentPage.processPage.elementUrls["_cBNW5VYfEee_gcc8A_7avQ"] = ["Artefato com os critérios de checagem e efeitos desejados","Questao2Plugin/workproducts/artefato_com_os_critrios_de_checagem_e_efeitos_desejados_50E5F774.html"];
+contentPage.processPage.elementUrls["_St0K0FYdEee_gcc8A_7avQ"] = ["Apontar equipamentos e ferramentas de checagem","Questao2Plugin/tasks/apontar_equipamentos_e_ferramentas_de_checagem_DDCAD83.html"];
+contentPage.processPage.elementUrls["_mtIgUVYaEee_gcc8A_7avQ"] = ["SP 1.1 Eleger Produtos de Trabalho para Checagem","Questao2Plugin/deliveryprocesses/SP 1.1 Eleger Produtos de Trabalho para Checagem_3DD7895B.html"];
+contentPage.processPage.elementUrls["_JpOk4FYdEee_gcc8A_7avQ"] = ["Apontar os métodos de checagem que estão acessíveis para utilização","Questao2Plugin/tasks/apontar_os_mtodos_de_checagem_que_esto_acessveis_para_utilizao_213744BE.html"];
+contentPage.processPage.elementUrls["_-pUzsFYaEee_gcc8A_7avQ"] = ["SP 2.1 Efetuar a Autenticação","Questao2Plugin/deliveryprocesses/SP 2.1 Efetuar a Autenticação_87BAF8F0.html"];
+contentPage.processPage.elementUrls["_qefxwVYdEee_gcc8A_7avQ"] = ["Analista de Teste","Questao2Plugin/roles/analista_de_teste_E6E75094.html"];
+contentPage.processPage.elementUrls["_U4TscFYdEee_gcc8A_7avQ"] = ["Obter equipamento de suporte e ambiente de checagem, tais como equipamentos e software de teste","Questao2Plugin/tasks/obter_equipamento_de_suporte_e_ambiente_de_checagem_tais_como_eq_1951543B.html"];
+contentPage.processPage.elementUrls["_PqXA4VYdEee_gcc8A_7avQ"] = ["Analista de Qualidade","Questao2Plugin/roles/analista_de_qualidade_8BAFE9CD.html"];
+contentPage.processPage.elementUrls["_bp5VAVYdEee_gcc8A_7avQ"] = ["Analista de Infraestrutura","Questao2Plugin/roles/analista_de_infraestrutura_79BAA613.html"];
+contentPage.processPage.elementUrls["_PqXA4FYdEee_gcc8A_7avQ"] = ["Estipular o ambiente de checagem","Questao2Plugin/tasks/estipular_o_ambiente_de_checagem_17B09FF2.html"];
+contentPage.processPage.elementUrls["_cBqC0VYfEee_gcc8A_7avQ"] = ["Artefato que cataloga os tipos de inspeções e os critérios de entradas e saídas desejados","Questao2Plugin/workproducts/artefato_que_cataloga_os_tipos_de_inspees_e_os_critrios_de_entra_60A87BF4.html"];
+contentPage.processPage.elementUrls["_Ff9CY1YdEee_gcc8A_7avQ"] = ["Cliente","Questao2Plugin/roles/cliente_94980719.html"];
+contentPage.processPage.elementUrls["_RKMb8FYdEee_gcc8A_7avQ"] = ["Analista de Infraestrutura","Questao2Plugin/roles/analista_de_infraestrutura_79BAA613.html"];
+contentPage.processPage.elementUrls["_lFMx1VYdEee_gcc8A_7avQ"] = ["Cliente","Questao2Plugin/roles/cliente_94980719.html"];
+contentPage.processPage.elementUrls["_cAwq8VYfEee_gcc8A_7avQ"] = ["Artefato de recursos de checagem acessíveis para reaproveitamento","Questao2Plugin/workproducts/artefato_de_recursos_de_checagem_acessveis_para_reaproveitamento_7754355B.html"];
+contentPage.processPage.elementUrls["_26k_UVYaEee_gcc8A_7avQ"] = ["SP 1.1 Eleger Produtos para Autenticação","Questao2Plugin/deliveryprocesses/SP 1.1 Eleger Produtos para Autenticação_86257C0E.html"];
+contentPage.processPage.elementUrls["_Ff9CYVYdEee_gcc8A_7avQ"] = ["Analista de Negócio","Questao2Plugin/roles/analista_de_negcio_C33CF753.html"];
+contentPage.processPage.elementUrls["_vxzl0FYaEee_gcc8A_7avQ"] = ["SP 2.2 Efetuar Inspeções por Pares","Questao2Plugin/deliveryprocesses/SP 2.2 Efetuar Inspeções por Pares_1189515F.html"];
+contentPage.processPage.elementUrls["_AiIoEVYeEee_gcc8A_7avQ"] = ["Catálogo de produtos a serem checados","Questao2Plugin/workproducts/catlogo_de_produtos_a_serem_checados_37E4B5DF.html"];
+contentPage.processPage.elementUrls["_psPVwVYaEee_gcc8A_7avQ"] = ["SP 1.2 Estipular o Ambiente de Checagem","Questao2Plugin/deliveryprocesses/SP 1.2 Estipular o Ambiente de Checagem_8D4C2A9D.html"];
+contentPage.processPage.elementUrls["_cBqC1FYfEee_gcc8A_7avQ"] = ["Artefato que cataloga os tipos de inspeções e os critérios de entradas e saídas desejados e checklist de veracidade","Questao2Plugin/workproducts/artefato_que_cataloga_os_tipos_de_inspees_e_os_critrios_de_entra_3F94BFE0.html"];
+contentPage.processPage.elementUrls["_r_lRYVYaEee_gcc8A_7avQ"] = ["SP 1.3 Estipular Táticas e Critérios de Checagem","Questao2Plugin/deliveryprocesses/SP 1.3 Estipular Táticas e Critérios de Checagem_B455DD85.html"];
+contentPage.processPage.elementUrls["_vjRTMFYdEee_gcc8A_7avQ"] = ["Registrar os efeitos das atividades de checagem","Questao2Plugin/tasks/registrar_os_efeitos_das_atividades_de_checagem_73C30EB4.html"];
+contentPage.processPage.elementUrls["_ZbLasVYdEee_gcc8A_7avQ"] = ["Analista de Teste","Questao2Plugin/roles/analista_de_teste_E6E75094.html"];
+contentPage.processPage.elementUrls["_G6vlYFYdEee_gcc8A_7avQ"] = ["Apontar os critérios a serem entregues por cada produto de trabalho escolhido","Questao2Plugin/tasks/apontar_os_critrios_a_serem_entregues_por_cada_produto_de_trabal_60D3A988.html"];
+contentPage.processPage.elementUrls["_cBNW4VYfEee_gcc8A_7avQ"] = ["Artefato com os critérios de checagem","Questao2Plugin/workproducts/artefato_com_os_critrios_de_checagem_830E7FC9.html"];
+contentPage.processPage.elementUrls["_gpajAFYdEee_gcc8A_7avQ"] = ["Efetuar os papéis designados na vistoria por pares","Questao2Plugin/tasks/efetuar_os_papis_designados_na_vistoria_por_pares_7BD8B67D.html"];
+contentPage.processPage.elementUrls["_cA6b8VYfEee_gcc8A_7avQ"] = ["Artefato de equipamentos, softwares e ferramentas de checagem fundamentais para montagem do ambiente de checagem","Questao2Plugin/workproducts/artefato_de_equipamentos_softwares_e_ferramentas_de_checagem_fun_310C7E72.html"];
+contentPage.processPage.elementUrls["_PqXA5VYdEee_gcc8A_7avQ"] = ["Analista de Teste","Questao2Plugin/roles/analista_de_teste_E6E75094.html"];
+contentPage.processPage.elementUrls["_JpOk41YdEee_gcc8A_7avQ"] = ["Analista de Teste","Questao2Plugin/roles/analista_de_teste_E6E75094.html"];
+contentPage.processPage.elementUrls["_qefxwFYdEee_gcc8A_7avQ"] = ["Registrar os dados relacionados à organização, execução e efeitos das inspeções por pares","Questao2Plugin/tasks/registrar_os_dados_relacionados__organizao_execuo_e_efeitos_das__54BE28BB.html"];
+contentPage.processPage.elementUrls["_1H0oQVYaEee_gcc8A_7avQ"] = ["SP 3.2 Analisar os Efeitos das Verificações e Apontar Ações Corretivas","Questao2Plugin/deliveryprocesses/SP 3.2 Analisar os Efeitos das Verificações e Apontar Ações Corr_48733030.html"];
+contentPage.processPage.elementUrls["_hsLnkFYdEee_gcc8A_7avQ"] = ["Estipular e conservar checklists para assegurar que os produtos de trabalho sejam revisados de forma eficaz","Questao2Plugin/tasks/estipular_e_conservar_checklists_para_assegurar_que_os_produtos__EC265534.html"];
+contentPage.processPage.elementUrls["_HEVw0VYeEee_gcc8A_7avQ"] = ["Catálogo de produtos a serem checados","Questao2Plugin/workproducts/catlogo_de_produtos_a_serem_checados_37E4B5DF.html"];
+contentPage.processPage.elementUrls["_gpajA1YdEee_gcc8A_7avQ"] = ["Cliente","Questao2Plugin/roles/cliente_94980719.html"];
+contentPage.processPage.elementUrls["_fDzXcVYaEee_gcc8A_7avQ"] = ["ProcessoTrabalho2","Questao2Plugin/deliveryprocesses/ProcessoTrabalho2_3C7A1264.html"];
+contentPage.processPage.elementUrls["_U4TscVYdEee_gcc8A_7avQ"] = ["Gerente","Questao2Plugin/roles/gerente_1DA43E86.html"];
+contentPage.processPage.elementUrls["_lFMx01YdEee_gcc8A_7avQ"] = ["Analista de Teste","Questao2Plugin/roles/analista_de_teste_E6E75094.html"];
+contentPage.processPage.elementUrls["_PqXA41YdEee_gcc8A_7avQ"] = ["Analista de Requisitos","Questao2Plugin/roles/analista_de_requisitos_7FB3F17C.html"];
+contentPage.processPage.elementUrls["_zMs_YVYaEee_gcc8A_7avQ"] = ["SP 3.1 Efetuar a Checagem","Questao2Plugin/deliveryprocesses/SP 3.1 Efetuar a Checagem_CCD642D1.html"];
+contentPage.processPage.elementUrls["_bp5VAFYdEee_gcc8A_7avQ"] = ["Apontar todos os equipamentos e componentes de ambiente precisos para segurar a checagem","Questao2Plugin/tasks/apontar_todos_os_equipamentos_e_componentes_de_ambiente_precisos_3FD1ED10.html"];
+contentPage.processPage.elementUrls["_aO6IQVYeEee_gcc8A_7avQ"] = ["Artefato que retrata e cataloga os produtos e critérios mais valiosos que farão parte do plano do projeto","Questao2Plugin/workproducts/artefato_que_retrata_e_cataloga_os_produtos_e_critrios_mais_vali_7BF91967.html"];
+contentPage.processPage.elementUrls["_ZbLasFYdEee_gcc8A_7avQ"] = ["Apontar os efeitos desejados, exequíveis tolerâncias autorizadas na verificação e outros critérios para atender os critérios","Questao2Plugin/tasks/apontar_os_efeitos_desejados_exequveis_tolerncias_autorizadas_na_1E950E5E.html"];
+contentPage.processPage.elementUrls["_r03HwVYdEee_gcc8A_7avQ"] = ["Analista de Teste","Questao2Plugin/roles/analista_de_teste_E6E75094.html"];
+contentPage.processPage.elementUrls["_RKCq8FYdEee_gcc8A_7avQ"] = ["Apontar os recursos de checagem acessíveis para reaproveitamento e remodelação","Questao2Plugin/tasks/apontar_os_recursos_de_checagem_acessveis_para_reaproveitamento__30F8078A.html"];
+contentPage.processPage.elementUrls["_KwmAIVYeEee_gcc8A_7avQ"] = ["Catálogo de todos os métodos exequíveis para checagem","Questao2Plugin/workproducts/catlogo_de_todos_os_mtodos_exequveis_para_checagem_35AFF5CF.html"];
+contentPage.processPage.elementUrls["_cBgR0VYfEee_gcc8A_7avQ"] = ["Artefato que catalogue os tipos de inspeções por pares que serão feitos","Questao2Plugin/workproducts/artefato_que_catalogue_os_tipos_de_inspees_por_pares_que_sero_fe_90A3A8CE.html"];
+contentPage.processPage.elementUrls["_TSXz8VYeEee_gcc8A_7avQ"] = ["Artefato que descreve qual método será utilizado para checagem","Questao2Plugin/workproducts/artefato_que_descreve_qual_mtodo_ser_utilizado_para_checagem_71E5E748.html"];
+contentPage.processPage.elementUrls["_M-kwEFYdEee_gcc8A_7avQ"] = ["Efetuar o mapeamento dos produtos de trabalho a serem checados, os critérios a serem entregues e os métodos a serem utilizados para a integração com o plano de projeto","Questao2Plugin/tasks/efetuar_o_mapeamento_dos_produtos_de_trabalho_a_serem_checados_o_9C6DAA7F.html"];
+contentPage.processPage.elementUrls["_HEVw1FYeEee_gcc8A_7avQ"] = ["Catálogo de junções dos critérios a serem checados e os produtos de trabalho","Questao2Plugin/workproducts/catlogo_de_junes_dos_critrios_a_serem_checados_e_os_produtos_de__152F4805.html"];
+contentPage.processPage.elementUrls["_5ZobsVYaEee_gcc8A_7avQ"] = ["SP 1.2 Estipular o Ambiente de Autenticação","Questao2Plugin/deliveryprocesses/SP 1.2 Estipular o Ambiente de Autenticação_EFD83070.html"];
+contentPage.processPage.elementUrls["_eKcmMVYdEee_gcc8A_7avQ"] = ["Analista de Teste","Questao2Plugin/roles/analista_de_teste_E6E75094.html"];
+contentPage.processPage.elementUrls["_xjtoUVYaEee_gcc8A_7avQ"] = ["SP 2.3 Analisar Dados da Vistoria por Pares","Questao2Plugin/deliveryprocesses/SP 2.3 Analisar Dados da Vistoria por Pares_DD1F0723.html"];
+contentPage.processPage.elementUrls["_cAm58VYfEee_gcc8A_7avQ"] = ["Artefato de critérios para o ambiente de checagem","Questao2Plugin/workproducts/artefato_de_critrios_para_o_ambiente_de_checagem_965E93E1.html"];
+contentPage.processPage.elementUrls["_PqXA51YdEee_gcc8A_7avQ"] = ["Arquiteto","Questao2Plugin/roles/arquiteto_F54125DC.html"];
+contentPage.processPage.elementUrls["_eKcmMFYdEee_gcc8A_7avQ"] = ["Prescrever que tipo de vistoria por pares que será executada","Questao2Plugin/tasks/prescrever_que_tipo_de_vistoria_por_pares_que_ser_executada_564F2247.html"];
+contentPage.processPage.elementUrls["_jOV94FYdEee_gcc8A_7avQ"] = ["Responsabilizar papéis para a vistoria por pares","Questao2Plugin/tasks/responsabilizar_papis_para_a_vistoria_por_pares_4AFDDC62.html"];
+contentPage.processPage.elementUrls["_72Da0VYaEee_gcc8A_7avQ"] = ["SP 1.3 Estipular Táticas e Critérios de Autenticação.","Questao2Plugin/deliveryprocesses/SP 1.3 Estipular Táticas e Critérios de Autenticação._DCF5B7B3.html"];
+contentPage.processPage.elementUrls["_gpajAVYdEee_gcc8A_7avQ"] = ["Analista de Requisitos","Questao2Plugin/roles/analista_de_requisitos_7FB3F17C.html"];
+contentPage.processPage.elementUrls["_lFMx0VYdEee_gcc8A_7avQ"] = ["Analista de Requisitos","Questao2Plugin/roles/analista_de_requisitos_7FB3F17C.html"];
+contentPage.processPage.elementUrls["_nCEHIFYdEee_gcc8A_7avQ"] = ["Apontar e registrar os falhas e outros aspectos sobre o produto de trabalho","Questao2Plugin/tasks/apontar_e_registrar_os_falhas_e_outros_aspectos_sobre_o_produto__A6C1E4D4.html"];
+contentPage.processPage.elementUrls["_ofV9cFYdEee_gcc8A_7avQ"] = ["Assegurar que os critérios de saída para a vistoria por pares estão sendo entregues","Questao2Plugin/tasks/assegurar_que_os_critrios_de_sada_para_a_vistoria_por_pares_esto_5AD8F1CA.html"];
